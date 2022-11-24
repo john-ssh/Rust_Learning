6a4cd9ff3966bbdc3d7c5c5e6b9d4ce6c97944e5
john-ssh <th3gentl3lm3nt@gmail.com>
2022-11-24 16:49:29 -0300

    Files Upload

---
+++ target/debug/Rust_Learning.d
@@ -0,0 +1,1 @@
+/home/john/Documents/Rust_Learning/target/debug/Rust_Learning: /home/john/Documents/Rust_Learning/src/cli.rs /home/john/Documents/Rust_Learning/src/main.rs
---
+++ target/debug/deps/Rust_Learning-f1f08d793fd1f6d0.d
@@ -0,0 +1,6 @@
+/home/john/Documents/Rust_Learning/target/debug/deps/Rust_Learning-f1f08d793fd1f6d0: src/main.rs src/cli.rs
+
+/home/john/Documents/Rust_Learning/target/debug/deps/Rust_Learning-f1f08d793fd1f6d0.d: src/main.rs src/cli.rs
+
+src/main.rs:
+src/cli.rs:
